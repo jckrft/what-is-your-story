812b918986210a323cee43d90b87279840b1e57c
jckrft <jckrft@gmail.com>
2021-12-11 21:15:18 -0500

    scaffold user, topic, and response and added seed data, migrated

---
+++ db/migrate/20211210221404_create_responses.rb
@@ -0,0 +1,11 @@
+class CreateResponses < ActiveRecord::Migration[6.1]
+  def change
+    create_table :responses do |t|
+      t.text :response
+      t.references :user, null: false, foreign_key: true
+      t.references :topic
+
+      t.timestamps
+    end
+  end
+end
