e5125b1954f37c4af83b47e5e1cdf6a03225bce2
jckrft <jckrft@gmail.com>
2021-12-12 23:54:55 -0500

    modal for sign up

--- client/src/screens/SignIn.jsx
+++ client/src/screens/SignIn.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
 
 export default function SignIn(props) {
   const [formData, setFormData] = useState({
@@ -17,13 +21,39 @@ export default function SignIn(props) {
     }))
   }
 
-  
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+
   return (
     <div>
+      <div>
+      <Button onClick={handleOpen}>sign in</Button>
+        <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography>sign in</Typography>
+            <Typography>
       <form onSubmit={(ev) => {
         ev.preventDefault(); handleLogin(formData)
       }}>
-        <h3>sign in</h3>
         <label>username:
           <input
             type='text'
@@ -43,7 +73,11 @@ export default function SignIn(props) {
         <Link to='/signup'>sign up</Link>
         <br />
         <button>submit</button>
-      </form>
+              </form>
+              </Typography>
+          </Box>
+        </Modal>
+        </div>
     </div>
   )
 }
--- client/src/screens/SignUp.jsx
+++ client/src/screens/SignUp.jsx
@@ -1,4 +1,11 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 export default function SignUp(props) {
   const [formData, setFormData] = useState({
@@ -16,39 +23,70 @@ export default function SignUp(props) {
       [name]: value
     }))
   }
+
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   
   return (
     <div>
-      <form onSubmit={(ev) => {
-        ev.preventDefault();
-        handleRegister(formData)
-      }}>
-        <h3>sign up</h3>
-        <label>username:
-          <input
+        <Button onClick={handleClickOpen}>
+        sign up
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>sign up</DialogTitle>
+        <DialogContent>
+    
+            <TextField
+              autoFocus
+            margin="dense"
             type='text'
-            name='username'
+              name='username'
+              label='username'
             value={username}
-            onChange={handleChange} />
-        </label>
-        <br />
-        <label>email:
-          <input
+                  onChange={handleChange} 
+
+              />
+            <br />
+          <TextField
+                          autofocus
+                          margin="dense"
+        label='email'
+          
             type='text'
             name='email'
             value={email}
-            onChange={handleChange} />
-        </label>
-        <label>password:
-          <input
+            onChange={handleChange} 
+          
+            />
+          <TextField
+                          autofocus
+                          margin="dense"
+        label='password'
+          
             type='password'
             name='password'
             value={password}
-            onChange={handleChange} />
-        </label>
+            onChange={handleChange} 
+            
+            />
         <br />
-        <button>submit</button>
-      </form>
+
+        </DialogContent>
+        <DialogActions>
+        <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={(ev) => {
+        ev.preventDefault();
+        handleRegister(formData)
+      }}>Submit</Button>
+        </DialogActions>
+        </Dialog>
     </div>
   )
 }
