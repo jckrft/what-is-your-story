4a1b5847736e7d3e5e1bd9a6a7479ab7d2e12045
jckrft <jckrft@gmail.com>
2021-12-15 22:51:00 -0500

    response create form

--- client/src/screens/ResponseCreate.jsx
+++ client/src/screens/ResponseCreate.jsx
@@ -1,39 +1,50 @@
-import { useState } from 'react';
-import { useParams, useHistory } from 'react-router-dom';
-import {postResponse} from '../services/responses'
+import { useState } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import { postResponse } from "../services/responses";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import '../assets/css/ResponseCreate.css'
 
 export default function ResponseCreate() {
   const [formData, setFormData] = useState({
-    response: ''
-  })
+    response: "",
+  });
 
   const { response } = formData;
   const { id } = useParams();
   const history = useHistory();
 
   const handleChange = (e) => {
-    const { value } = e.target 
-    setFormData({response: value})
-  }
+    const { value } = e.target;
+    setFormData({ response: value });
+  };
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
-    await postResponse(id, formData)
-    history.push(`/topics/${id}`)
-  }
+    e.preventDefault();
+    await postResponse(id, formData);
+    history.push(`/topics/${id}`);
+  };
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <label> add response
-          <input
-            type='text'
-            value={response}
-            onChange={handleChange}
-          />
-        </label>
-        <button>submit</button>
+      <form onSubmit={handleSubmit} className="response-form">
+        <TextField
+          className="response-textfield"
+          autoFocus
+          margin="dense"
+          type="text"
+          name="response"
+          label="response"
+          id={"response"}
+          fullWidth
+          multiline
+          rows={4}
+          variant="standard"
+          value={response}
+          onChange={handleChange}
+        />
+        <Button>submit</Button>
       </form>
     </div>
-  )
+  );
 }
